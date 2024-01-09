447b01fd944cca89e209bff78a631cba77e4b49c
vaishnavi2324 <155826682+vaishnavi2324@users.noreply.github.com>
2024-01-09 12:58:29 +0530

    passwordgenerator.py
    PASSWORD GENERATOR
    
    A password generator is a useful tool that generates strong and
    random passwords for users. This project aims to create a
    password generator application using Python, allowing users to
    specify the length and complexity of the password.
    
    User Input: Prompt the user to specify the desired length of the
    password.
    
    Generate Password: Use a combination of random characters to
    generate a password of the specified length.
    
    Display the Password: Print the generated password on the screen.

---
+++ passwordgenerator.py
@@ -0,0 +1,22 @@
+import random
+import string
+
+def generate_password(length):
+    characters = string.ascii_letters + string.digits + string.punctuation
+    password = ' '.join(random.choice(characters) for _ in range(length))
+    
+    return password
+    
+def password():
+    try:
+        length=int(input("Enter the desired password length: "))
+        
+        if length <= 0:
+            print("Password length must be a positive integer.")
+        else:
+            password=generate_password(length)
+            print("Generated Password:",password)
+    except ValueError:
+        print("Invalid input. Please enter a valid positive integer for password length.")
+        
+password()
